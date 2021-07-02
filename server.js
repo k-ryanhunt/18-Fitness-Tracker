63b6695c46ef9445a1e36fccd4ab402e8072e7d4
K Ryan <hunt.kelsearyan@gmail.com>
2021-07-02 09:19:55 -0600

    Working on server and dependencies

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitness-Tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
